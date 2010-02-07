5138cf93cb852971069ad855de7502192ceb83fd
Michael Brennan <michael@michaelbrennan.net>
2010-02-06 19:15:59 -0500

    Apply first round of SRP refactoring
    Created CalculationMode and SizeInformation types.
    Moved several pieces of code out of Main and into seperate methods.
    Moved DescendingComparer to its own file

---
+++ DirectorySizeCounter/DescendingComparer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace DirectorySizeCounter
+{
+    internal class DescendingComparer<T> : IComparer<T> where T : IComparable<T>
+    {
+        public int Compare(T x, T y)
+        {
+            return y.CompareTo(x);
+        }
+    }
+}
