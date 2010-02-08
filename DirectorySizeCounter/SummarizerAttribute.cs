3a7b7d465815a29026b8f2ad4054c87ef42d55c9
Michael Brennan <michael@michaelbrennan.net>
2010-02-08 11:45:19 -0500

    Add support for custom calculators and serializers

---
+++ DirectorySizeCounter/SummarizerAttribute.cs
@@ -0,0 +1,9 @@
+namespace DirectorySizeCounter
+{
+	internal class SummarizerAttribute : CommandLineIdentifierAttribute
+	{
+		public SummarizerAttribute(string commandLineIdentifier, string description) : base(commandLineIdentifier, description)
+		{
+		}
+	}
+}
