a5c2a884a2ce4673512ec20939fe313141a0d8ba
Michael Brennan <michael@michaelbrennan.net>
2010-02-08 11:27:52 -0500

    Apply second round of SRP refactoring

--- DirectorySizeCounter/DescendingComparer.cs
+++ DirectorySizeCounter/DescendingComparer.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace DirectorySizeCounter
 {
-    internal class DescendingComparer<T> : IComparer<T> where T : IComparable<T>
-    {
-        public int Compare(T x, T y)
-        {
-            return y.CompareTo(x);
-        }
-    }
+	internal class DescendingComparer<T> : IComparer<T> where T : IComparable<T>
+	{
+		public int Compare(T x, T y)
+		{
+			return y.CompareTo(x);
+		}
+	}
 }
