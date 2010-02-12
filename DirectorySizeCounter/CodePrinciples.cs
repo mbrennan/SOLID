48c4fa5403e3ad4081e336aa6407115508ec04f4
Michael Brennan <michael@michaelbrennan.net>
2010-02-12 17:18:30 -0500

    Add SOLID Code Principles reference to project

---
+++ DirectorySizeCounter/CodePrinciples.cs
@@ -0,0 +1,24 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+// SOLID Principles
+///////////////////////////////////////////////////////////////////////////////
+//
+// SRP: Single Responsibility Principle
+// - There should never be more than one reason for an object to change
+//
+// OCP: Open-Closed Principle
+// - An object should be open for extension but closed for modification
+//
+// LSP: Liskov Substitution Principle
+// - References to objects must be able to use a more specialized object
+//   without knowing it
+//
+// ISP: Interface Segregation Principle
+// - Objects should not be forced to depend upon interfaces they do not use
+//
+// DIP: Dependency Inversion Principle
+// - Higher-level modules should not depend upon lower level modules; both
+//   should depend upon abstractions
+// - Abstractions should not depend upon details; details should depend upon
+//   abstractions
+// 
+///////////////////////////////////////////////////////////////////////////////
